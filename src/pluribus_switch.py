69064e31cf01acf98f3b51005ddd41b4ed82f0d0
behram mistree <bmistree@gmail.com>
2014-07-07 16:03:02 -0700

    Listening for feature responses and echo responses.

--- src/pluribus_switch.py
+++ src/pluribus_switch.py
@@ -4,9 +4,10 @@ import time
 from ryu.base import app_manager
 from ryu.controller import mac_to_port
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls, set_ev_handler
 from ryu.ofproto.ofproto_v1_3_parser import OFPInstructionGotoTable
+from ryu.ofproto.ofproto_v1_3_parser import OFPEchoRequest
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.controller import conf_switch
@@ -20,7 +21,8 @@ class PluribusSwitch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(PluribusSwitch, self).__init__(*args, **kwargs)
         self.switch_dp = None
-
+        self.switch_num_tables = None
+        
     def try_install(self):
         '''
         Simple example method to try installing a dummy rule
@@ -30,17 +32,41 @@ class PluribusSwitch(app_manager.RyuApp):
         if self.switch_dp is None:
             print 'No attached switch'
             return
-        
+
+        # try to install a new flow mod
         match = self.switch_dp.ofproto_parser.OFPMatch(
             in_phy_port=1)
         priority = self.switch_dp.ofproto.OFP_DEFAULT_PRIORITY
         table_id = 1
         # test is to add a goto
         instructions = [OFPInstructionGotoTable(table_id+1)]
-
         self.add_flow_mod(match,instructions,priority,table_id)
         
-        
+    def send_feature_request(self):
+        '''
+        Send a request to get switch's features
+        '''
+        if self.switch_dp is not None:
+            feature_msg = self.switch_dp.ofproto_parser.OFPFeaturesRequest(
+                self.switch_dp)
+            self.switch_dp.send_msg(feature_msg)
+        else:
+            # not yet initialized
+            assert False
+
+    def send_echo_request(self):
+        '''
+        Send an echo to switch
+        '''
+        if self.switch_dp is not None:
+            print '\nSending echo request\n'
+            echo_msg = OFPEchoRequest(self.switch_dp)
+            self.switch_dp.send_msg(echo_msg)
+        else:
+            # not yet initialized
+            assert False
+
+            
     def add_flow_mod(self,match,instructions,priority,table_id):
         flow_mod_msg = self.switch_dp.ofproto_parser.OFPFlowMod(
             self.switch_dp, # datapath
@@ -67,49 +93,20 @@ class PluribusSwitch(app_manager.RyuApp):
         self.switch_dp.send_msg(flow_mod_msg)
 
         
-    @set_ev_cls(conf_switch.EventConfSwitchSet)
-    def conf_switch_set_handler(self, ev):
-        print '\n\nGot envent set\n\n'
-
-    @set_ev_cls(conf_switch.EventConfSwitchDel)
-    def conf_switch_del_handler(self, ev):
-        print '\n\nGot envent del\n\n'
-
-    @set_ev_cls(conf_switch.EventConfSwitchDel)
-    def conf_switch_del_handler(self, ev):
-        print '\n\nGot envent del\n\n'
-
-    @set_ev_cls(conf_switch.EventConfSwitchDel)
-    def dp_switch_del_handler(self, ev):
-        print '\n\nGot envent del\n\n'
-
-    @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
-    def dp_evt (self,ev):
-        if ev.dp.state == MAIN_DISPATCHER:
-            print 'Got new switch'
-            self.switch_dp = ev.dp
-            print self.switch_dp.ofproto
-            t = threading.Thread(target=self.try_install)
-            t.start()
-
-
-            
-    # def get_switch_features(self):
-    #     '''Gets number of available tables.
-    #     '''
-    #     m = dp.ofproto_parser.OFPFeaturesRequest(dp)
-    #     dp.send_msg(m)
-            
+    @set_ev_cls(ofp_event.EventOFPEchoRequest,[MAIN_DISPATCHER])
+    def recv_echo_response(self, ev):
+        print '\nReceived echo response\n'
         
-    # def add_flow(self, datapath, in_port, dst, actions,table):
-    #     ofproto = datapath.ofproto
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures,[CONFIG_DISPATCHER])
+    def recv_switch_features_response(self,ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        self.switch_num_tables = msg.n_tables
 
-    #     match = datapath.ofproto_parser.OFPMatch(
-    #         in_port=in_port, dl_dst=haddr_to_bin(dst))
-
-    #     mod = datapath.ofproto_parser.OFPFlowMod(
-    #         datapath=datapath, match=match, cookie=0,
-    #         command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-    #         priority=ofproto.OFP_DEFAULT_PRIORITY,
-    #         flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
-    #     datapath.send_msg(mod)
+    @set_ev_cls(dpset.EventDP, [CONFIG_DISPATCHER])
+    def dp_evt (self,ev):
+        print 'Got new switch'
+        self.switch_dp = ev.dp
+        print self.switch_dp.ofproto
+        t = threading.Thread(target=self.try_install)
+        t.start()
