b42df0fa9a95b29f679e2bb236abf83c72e6e8b4
behram mistree <bmistree@gmail.com>
2014-07-07 10:39:51 -0700

    Updated pluribus switch add_flow_mod to agree with ryu spec.  Note: dumping flow table still suggests that not actually installing flow mod.

--- src/pluribus_switch.py
+++ src/pluribus_switch.py
@@ -6,6 +6,7 @@ from ryu.controller import mac_to_port
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
+from ryu.ofproto.ofproto_v1_3_parser import OFPInstructionGotoTable
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.controller import conf_switch
@@ -21,7 +22,10 @@ class PluribusSwitch(app_manager.RyuApp):
         self.switch_dp = None
 
     def try_install(self):
-        time.sleep(5)
+        '''
+        Simple example method to try installing a dummy rule
+        '''
+        time.sleep(10)
         
         if self.switch_dp is None:
             print 'No attached switch'
@@ -30,31 +34,38 @@ class PluribusSwitch(app_manager.RyuApp):
         match = self.switch_dp.ofproto_parser.OFPMatch(
             in_phy_port=1)
         priority = self.switch_dp.ofproto.OFP_DEFAULT_PRIORITY
-        # should just be a drop??
-        actions = []
         table_id = 1
+        # test is to add a goto
+        instructions = [OFPInstructionGotoTable(table_id+1)]
+
         self.add_flow_mod(match,instructions,priority,table_id)
         
         
     def add_flow_mod(self,match,instructions,priority,table_id):
-        flow_mod = self.switch_dp.ofproto_parser.OFPFlowMod(
-            # default
-            datapath=self.switch_dp, cookie=0,
-            cookie_mask=0,
+        flow_mod_msg = self.switch_dp.ofproto_parser.OFPFlowMod(
+            self.switch_dp, # datapath
+            0, # cookie
+            0, # cookie_mask
 
-            table_id=table_id,
+            table_id,
                         
-            command=self.switch_dp.ofproto.OFPFC_ADD,
-            idle_timeout=0, hard_timeout=0,
+            self.switch_dp.ofproto.OFPFC_ADD, # command
+            0, # idle_timeout
+            0, # hard_timeout
 
-            priority=priority,
+            priority, # priority
 
-            buffer_id = self.switch_dp.ofproto.OFP_NO_BUFFER,
-            out_port=0,out_group=0,flags=0,
+            self.switch_dp.ofproto.OFP_NO_BUFFER, # buffer_id
+            0, # out_port
+            0, # out_group
+            0, # flags
             
-            match = match,
-            instructions=instructions)
-        
+            match,
+            instructions)
+
+        print '\nSending flow mod\n'
+        self.switch_dp.send_msg(flow_mod_msg)
+
         
     @set_ev_cls(conf_switch.EventConfSwitchSet)
     def conf_switch_set_handler(self, ev):
