1a8e4e814fd9947e881b0aa195304c3890efe9c5
behram mistree <bmistree@gmail.com>
2014-07-10 15:47:36 -0700

    Interposing on handshake.

--- principals/dummy.py
+++ principals/dummy.py
@@ -10,6 +10,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import HANDSHAKE_DISPATCHER, set_ev_cls
+#from ryu.controller.handler import set_ev_handler
 from ryu.ofproto.ofproto_v1_3_parser import OFPEchoRequest,OFPEchoReply
 from ryu.ofproto import ofproto_v1_3
 from ryu.controller import dpset
@@ -17,6 +18,11 @@ from ryu.controller import dpset
 class PluribusSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     _CONTEXTS = {'dpset': dpset.DPSet}
+
+    @set_ev_cls(ofp_event.EventOFPHello, 
+                [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def recv_hello(self, ev):
+        print '\n\nReceived hello\n\n'
 
     
     @set_ev_cls(ofp_event.EventOFPEchoReply,
@@ -31,3 +37,21 @@ class PluribusSwitch(app_manager.RyuApp):
         print '\n\nReceived echo request\n\n'
 
         
+    @set_ev_cls(ofp_event.EventOFPErrorMsg,
+                [HANDSHAKE_DISPATCHER,CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        
+        if self.state not in SwitchState.RUNNING:
+            # no graceful retries or anything if get an error while
+            # setting up head tables, getting port descriptors, etc.
+            # Just fail.
+            pluribus_logger.error(
+                'OFPErrorMsg received during initialization: ' +
+                ('type=0x%02x code=0x%02x  ' % (msg.type, msg.code)) +
+                'QUITTING')
+            assert False
+
+        pluribus_logger.error(
+            'Received ofp error.  Must finish handler method.')
+
