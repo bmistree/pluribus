6e5cc1efc9dbaaa0b27ccbe1e2def82f7c7596ea
behram mistree <bmistree@gmail.com>
2014-07-14 16:31:31 -0700

    Adding code to try to saturate number of packets through switch.  Counter keeps track of number of packets and periodically reports throughput stats.

---
+++ saturate/server/Counter.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+   Every polling period, announce throughput of packets received over
+   that period.
+ */
+
+public Counter extends Thread
+{
+    public final AtomicInteger counter;
+    private int previous_counter = 0;
+    private static final int POLLING_PERIOD_SECONDS = 10; // poll every 10s
+    private static final int SECONDS_TO_MS_MULTIPLIER = 1000;
+
+    
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            try
+            {
+                Thread.sleep(
+                    POLLING_PERIOD_SECONDS*SECONDS_TO_MS_MULTIPLIER);
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("\n\nSomething weird and wrong\n\n");
+                assert(false);
+                break;
+            }
+            int current_value = counter.get();
+            int num_received_in_period = current_value - previous_counter;
+
+            double throughput = calc_throughput(num_received_in_period);
+            previous_counter = current_value;
+
+            // report info
+            System.out.println(Double.toString(throughput) + " packets per second");
+        }
+    }
+
+    /**
+       @returns Packets per second.
+     */
+    private double calc_throughput(int num_received_in_period)
+    {
+        return ((double)num_received_in_period)/
+            ((double) POLLING_PERIOD_SECONDS);
+    }
+}
