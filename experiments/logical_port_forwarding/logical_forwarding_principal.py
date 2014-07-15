b86ec9e0b30fc124f16147b9ce96ecb60010a082
behram mistree <bmistree@gmail.com>
2014-07-15 16:51:07 -0700

    Added rules to allow traffic to pass between left and right switches.

--- experiments/logical_port_forwarding/logical_forwarding_principal.py
+++ experiments/logical_port_forwarding/logical_forwarding_principal.py
@@ -13,6 +13,11 @@ from ryu.controller import dpset
 
 ARP_ETH_TYPE = 0x0806
 
+PORT_1 = 1
+PORT_2 = 2
+PORT_3 = 3
+PORT_4 = 4
+
 class LogicalForwardingPrincipal(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     _CONTEXTS = {'dpset': dpset.DPSet}
@@ -45,7 +50,57 @@ class LogicalForwardingPrincipal(app_manager.RyuApp):
         self.send_flow_mod(
             datapath,table_id,arp_priority,match_arps_between,instructions)
 
+        # can't figure out how to pass in command line arguments to
+        # switch.  instead, just going to try to send sets of flow
+        # mods for both right and left switches.  For each switch, one
+        # set of flow mods will cause an exception on virtualization
+        # layer.  But, that shouldn't be too big of a deal for the
+        # controller.
+        self.left_switch_flow_mods(datapath)
+        self.right_switch_flow_mods(datapath)
+        
+        
 
+    def left_switch_flow_mods(self,datapath):
+        # left switch holds ports one and four
+        self.ingress_egress_switch(datapath,PORT_1,PORT_4)
+
+    def right_switch_flow_mods(self,datapath):
+        # right switch houses ports two and three
+        self.ingress_egress_switch(datapath,PORT_2,PORT_3)
+        
+    def ingress_egress_switch(self,datapath,left_port,right_port):
+        table_id = 0
+        priority = 10
+
+        # ingress on port left_port, output on right_port
+        in_left_out_right_match = datapath.ofproto_parser.OFPMatch(
+            in_port=left_port)
+        in_left_out_right_actions_to_apply = [
+            datapath.ofproto_parser.OFPActionOutput(right_port)]
+        self.send_flow_mod_with_actions_to_apply(
+            datapath,table_id,priority,in_left_out_right_match,
+            in_left_out_right_actions_to_apply)
+
+        # ingress on port right_port, output on left_port
+        in_right_out_left_match = datapath.ofproto_parser.OFPMatch(
+            in_port=right_port)
+        in_right_out_left_actions = [
+            datapath.ofproto_parser.OFPActionOutput(left_port)]
+        self.send_flow_mod_with_actions_to_apply(
+            datapath,table_id,priority,in_right_out_left_match,
+            in_right_out_left_to_apply)
+        
+
+    def send_flow_mod_with_actions_to_apply(
+        self,datapath,table_id,priority,match,actions_to_apply):
+        
+        instruction_actions = datapath.ofproto_parser.OFPInstructionActions(
+            datapath.ofproto.OFPIT_APPLY_ACTIONS,actions_to_apply)
+
+        self.send_flow_mod(datapath,table_id,priority,match,instruction_actions)
+        
+        
     def send_flow_mod(self,datapath,table_id,priority,match,instructions):
         flow_mod_msg = datapath.ofproto_parser.OFPFlowMod(
             datapath, # datapath
